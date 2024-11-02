b0076d58166cafe7ea20d4a3775c926c49789d92
Hernan Tapia <tapiahernan5@gmail.com>
2024-11-01 21:13:36 -0700

    added clerk forms

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-2xl font-bold mb-5">HOME</h1>
+    <p className="mb-5">Learning Clerk to impliment Authentication for users</p>
+    </>
+  );
+}
